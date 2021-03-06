7aa2ec689bfec5b6ca94e65b00246b2bfc52d14a
MYCOMPUTER\K018A1415 <zigokunohonou0919@gmail.com>
2021-03-07 04:46:52 +0900

    シーン追加と仮プレイヤー情報追加

---
+++ Team15/Assets/Scripte/Scene_Scripte/Title_SceneMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Title_SceneMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SceneManager.LoadScene("Title_Scene");
+            Debug.Log("シーン移行　タイトルシーンへ");
+        }//タイトルシーンに移行
+    }
+}
