c5622f383b3608e1f314701797cb8a6c6c7f3cae
MYCOMPUTER\K018A1415 <zigokunohonou0919@gmail.com>
2021-03-07 20:11:57 +0900

    プレイヤーショットを実装　現在の最適化

---
+++ Team15/Assets/Player_Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Bullet : MonoBehaviour
+{
+    public float BulletSpeed = 0.1f;//弾の速さ
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0f, BulletSpeed, 0f);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
