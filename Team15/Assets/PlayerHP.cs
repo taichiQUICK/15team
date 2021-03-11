7dd5b0ca94e680665ce3400b3c344a28c95e6299
iori0909 <iori.von.einzbern@gmail.com>
2021-03-11 17:12:23 +0900

    プレイヤーの残機表示、ハダシーン内のプレイヤーの画像差し替えとプレファブ化

---
+++ Team15/Assets/PlayerHP.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHP : MonoBehaviour
+{
+    int hp = 10;
+    Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.textComponent = GameObject.Find("Text").GetComponent<Text>();
+        this.textComponent.text = "× " + hp.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddScore()
+    {
+        this.hp -= 1;
+        this.textComponent.text = "× " + hp.ToString();
+    }
+}
